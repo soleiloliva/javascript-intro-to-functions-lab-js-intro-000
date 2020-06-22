6a04f4e0e673c8e961c21f2cd491c6e32704c5f0
Soleil Oliva <soleiloliva@gmail.com>
2020-06-22 21:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma() {
-
-}
+function sayHiToGrandma(string) {
+  if string === .toLowerCase()
+  return "I can't hear you"
